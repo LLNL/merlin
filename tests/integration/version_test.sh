72ebafebe35b6c06186ee4a01dd5e1d575ab0d91
Benjamin Bay <bay1@llnl.gov>
2020-01-02 10:52:54 -0800

    added negation

--- tests/integration/version_test.sh
+++ tests/integration/version_test.sh
@@ -5,10 +5,8 @@ grep CHANGELOG.md -m 1 -e "## \[\d\+\.\d\+\.\d\+\]" > GREP1
 grep merlin/__init__.py -m 1 -e "\"\d\+\.\d\+\.\d\+\"" > GREP2
 grep -o GREP1 -e "\d\+\.\d\+\.\d\+" > GREP3
 grep -o GREP2 -e "\d\+\.\d\+\.\d\+" > GREP4
-if cmp GREP3 GREP4
+if ! cmp GREP3 GREP4
 then 
-    :
-else
     echo "Error: merlin/__init__.py version different from CHANGELOG.md verison" 
     exit 1
 fi
@@ -19,30 +17,24 @@ git tag > TAGS
 grep "." TAGS | tail -1 > TAG
 grep CHANGELOG.md -m 1 -e "## \[\d\+\.\d\+\.\d\+\]" > GREP1
 grep -o GREP1 -e "\d\+\.\d\+\.\d\+" > GREP2
-if cmp TAG GREP2
+if ! cmp TAG GREP2
 then 
-    :
-else
     echo "Error: CHANGELOG.md verison different from most recent git tag" 
     exit 1
 fi
 
 
 # Ensure CHANGELOG has changed
-if [[ $(git diff HEAD..master -- CHANGELOG.md) ]]
+if ! [[ $(git diff HEAD..master -- CHANGELOG.md) ]]
 then
-    :
-else
     echo "Error: CHANGELOG.md has not been updated"
     exit 1
 fi
 
 
 # Ensure __init__.py has changed
-if [[ $(git diff HEAD..master -- merlin/__init__.py) ]]
+if ! [[ $(git diff HEAD..master -- merlin/__init__.py) ]]
 then
-    :
-else
     echo "Error: merlin/__init__.py has not been updated (version?)"
     exit 1
 fi
