5409fcd9725eabb6529f91d56c09e39ec643f384
Bay <bay1@llnl.gov>
2020-01-28 16:07:54 -0800

    added description, GitHub link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,11 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "description": "Machine learning for HPC workflows",
+    "github_user": "LLNL",
+    "github_repo": "merlin",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
