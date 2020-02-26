0e2986551fb8361db0a7c81022d52592cc569ba9
Bay <bay1@llnl.gov>
2020-02-25 16:06:00 -0800

    tutorial now appears to be working

--- docs/source/modules/hello_world/make_samples.py
+++ docs/source/modules/hello_world/make_samples.py
@@ -4,7 +4,7 @@ import names
 # argument parsing
 parser = argparse.ArgumentParser(description='Make some samples (names of people).')
 parser.add_argument('--number', type=int, action='store', help='the number of samples you want to make')
-parser.add_argument('--file', type=str, help='output file')
+parser.add_argument('--filepath', type=str, help='output file')
 args = parser.parse_args()
 
 # sample making
@@ -16,6 +16,6 @@ for i in range(args.number):
 for name in name_list:
     result += name + "\n"
 
-with open(args.file, "w") as f:
+with open(args.filepath, "w") as f:
     f.write(result)
 
