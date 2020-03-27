8037a70afe7cc8485b0a8b84eaf12d8adc0711d8
Benjamin Bay <bay1@llnl.gov>
2020-03-27 10:29:47 -0700

    updated paste_times

--- merlin/examples/workflows/null_spec/paste_times.sh
+++ merlin/examples/workflows/null_spec/paste_times.sh
@@ -2,15 +2,17 @@
 concurrencies=(1 2 4 8 16 32 64)
 samples=(1 10 100 1000 10000)
 read_path="/g/g13/bay1/null_results"
+DATA=my_data2.yaml
 
-touch my_data.yaml
+
+touch ${DATA}
 
 for c in "${concurrencies[@]}"
     do
     for s in "${samples[@]}"
         do
-        echo "c${c}_s${s} : " >> my_data.yaml
-        python3 task_script.py ${read_path}/c_$c/s_$s/*.log >> my_data.yaml
+        echo "c${c}_s${s} : " >> ${DATA}
+        python3 task_script.py ${read_path}/c_$c/s_$s/*.log >> ${DATA}
         done
     done
-perl -pi -e 's/ : \n/ : /g' my_data.yaml
+perl -pi -e 's/ : \n/ : /g' ${DATA}
