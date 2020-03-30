ebe66aeef956f08ca7be11fafd1c649b065cbc70
Benjamin Bay <bay1@llnl.gov>
2020-03-30 09:21:57 -0700

    added scripts

--- merlin/examples/workflows/null_spec/scripts/launch_jobs.py
+++ merlin/examples/workflows/null_spec/scripts/launch_jobs.py
@@ -1,17 +1,17 @@
-import subprocess
 import os
 import shutil
+import subprocess
 
 
 # ensure no workers or tasks from previous runs still exist
-subprocess.check_output("merlin stop-workers", shell=True).decode('ascii')
-subprocess.check_output("merlin purge null_spec.yaml -f", shell=True).decode('ascii')
+subprocess.check_output("merlin stop-workers", shell=True).decode("ascii")
+subprocess.check_output("merlin purge ../null_spec.yaml -f", shell=True).decode("ascii")
 
 # launch 35 merlin workflow jobs
 submit_path = "."
-concurrencies = [1,2,4,8,16,32,64]
-nodes =         [1,1,1,1, 1, 1, 2]
-samples = [1,10,100,1000,10000]
+concurrencies = [1, 2, 4, 8, 16, 32, 64]
+nodes = [1, 1, 1, 1, 1, 1, 2]
+samples = [1, 10, 100, 1000, 10000]
 output_path = "null_results/run_1"
 for i, concurrency in enumerate(concurrencies):
     c_name = os.path.join(output_path, f"c_{concurrency}")
@@ -26,7 +26,6 @@ for i, concurrency in enumerate(concurrencies):
         submit = f"submit_{nodes[i]}_node.sbatch"
         command = f"sbatch {submit} {sample} {int(concurrency/nodes[i])}"
         shutil.copyfile(os.path.join(submit_path, submit), submit)
-        lines = subprocess.check_output(command, shell=True).decode('ascii')
+        lines = subprocess.check_output(command, shell=True).decode("ascii")
         os.chdir(f"..")
     os.chdir(f"..")
-
--- merlin/examples/workflows/null_spec/scripts/read_output.py
+++ merlin/examples/workflows/null_spec/scripts/read_output.py
@@ -1,11 +1,10 @@
-
 import argparse
+import datetime
+import os
 import re
 import subprocess
-import time 
-import datetime 
-import os
-  
+import time
+
 
 # argument parsing
 parser = argparse.ArgumentParser(description="Make some samples (names of people).")
@@ -17,7 +16,9 @@ args = parser.parse_args()
 
 
 def single_task_times():
-    pre_lines = subprocess.check_output(f"grep \" succeeded in \" {args.logfile}", shell=True).decode('ascii')
+    pre_lines = subprocess.check_output(
+        f'grep " succeeded in " {args.logfile}', shell=True
+    ).decode("ascii")
 
     pre_list = pre_lines.strip().split("\n")
 
@@ -31,17 +32,23 @@ def single_task_times():
 
     print(str(task_durations))
 
+
 def merlin_run_time():
-    pre_line = subprocess.check_output(f"grep \"real\" {args.errfile}", shell=True).decode('ascii')
+    pre_line = subprocess.check_output(
+        f'grep "real" {args.errfile}', shell=True
+    ).decode("ascii")
     pre_line = pre_line.strip()
     matches = re.search(r"\d\.\d\d\d", pre_line)
     match = matches[0]
     result = float(match)
     print(f"c{args.c}_s{args.s} merlin run : " + str(result))
 
+
 def start_verify_time():
     try:
-        pre_line = subprocess.check_output(f"grep -m1 \"verify\" {args.logfile}", shell=True).decode('ascii')
+        pre_line = subprocess.check_output(
+            f'grep -m1 "verify" {args.logfile}', shell=True
+        ).decode("ascii")
         pre_line = pre_line.strip()
         matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
         match = matches[0]
@@ -50,25 +57,31 @@ def start_verify_time():
         print(f"c{args.c}_s{args.s} start verify : " + str(timestamp))
     except:
         print(f"c{args.c}_s{args.s} start verify : ERROR")
-    
+
+
 def start_run_workers_time():
-    pre_line = subprocess.check_output(f"grep -m1 \"\" {args.logfile}", shell=True).decode('ascii')
+    pre_line = subprocess.check_output(
+        f'grep -m1 "" {args.logfile}', shell=True
+    ).decode("ascii")
     pre_line = pre_line.strip()
     matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
     match = matches[0]
     element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
     timestamp = datetime.datetime.timestamp(element)
     print(f"c{args.c}_s{args.s} start run-workers : " + str(timestamp))
-    
+
+
 def start_sample1_time():
-    pre_line = subprocess.check_output(f"grep -m1 \"Executing step 'null_step'\" {args.logfile}", shell=True).decode('ascii')
+    pre_line = subprocess.check_output(
+        f"grep -m1 \"Executing step 'null_step'\" {args.logfile}", shell=True
+    ).decode("ascii")
     pre_line = pre_line.strip()
     matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
     match = matches[0]
     element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
     timestamp = datetime.datetime.timestamp(element)
     print(f"c{args.c}_s{args.s} start samp1 : " + str(timestamp))
-    
+
 
 def main():
     single_task_times()
@@ -76,6 +89,7 @@ def main():
     start_verify_time()
     start_run_workers_time()
     start_sample1_time()
+
 
 if __name__ == "__main__":
     main()
