0323261f3d89108dac0641026a41771fe769d5cd
Benjamin Bay <bay1@llnl.gov>
2020-03-30 14:53:42 -0700

    workflow updates

--- merlin/examples/workflows/null_spec/scripts/launch_jobs.py
+++ merlin/examples/workflows/null_spec/scripts/launch_jobs.py
@@ -5,6 +5,8 @@ import subprocess
 
 parser = argparse.ArgumentParser(description="Launch 35 merlin workflow jobs")
 parser.add_argument("run_id", type=int, help="The ID of this run")
+parser.add_argument("output_path", type=str, help="the output path")
+parser.add_argument("spec_path", type=str, help="path to the spec to run")
 args = parser.parse_args()
 
 # launch 35 merlin workflow jobs
@@ -12,8 +14,7 @@ script_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 concurrencies = [1, 2, 4, 8, 16, 32, 64]
 nodes = [1, 1, 1, 1, 1, 1, 2]
 samples = [1, 10, 100, 1000, 10000]
-output_path = f"null_results/run_{args.run_id}"
-output_path = os.path.abspath(output_path)
+output_path = os.path.join(args.output_path, f"run_{args.run_id}")
 os.makedirs(output_path, exist_ok=True)
 for i, concurrency in enumerate(concurrencies):
     c_name = os.path.join(output_path, f"c_{concurrency}")
@@ -28,6 +29,7 @@ for i, concurrency in enumerate(concurrencies):
         submit = f"submit_{nodes[i]}_node.sbatch"
         command = f"sbatch {submit} {sample} {int(concurrency/nodes[i])}"
         shutil.copyfile(os.path.join(script_path, submit), submit)
+        shutil.copyfile(args.spec_path, os.path.basename(args.spec_path))
         lines = subprocess.check_output(command, shell=True).decode("ascii")
         os.chdir(f"..")
     os.chdir(f"..")
