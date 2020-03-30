55f18944bf2485b48e244e08da3621f1a50a7a44
Benjamin Bay <bay1@llnl.gov>
2020-03-30 10:48:17 -0700

    improved scripts

--- merlin/examples/workflows/null_spec/scripts/launch_jobs.py
+++ merlin/examples/workflows/null_spec/scripts/launch_jobs.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import shutil
 import subprocess
@@ -6,17 +7,14 @@ parser = argparse.ArgumentParser(description="Launch 35 merlin workflow jobs")
 parser.add_argument("run_id", type=int, help="The ID of this run")
 args = parser.parse_args()
 
-# ensure no workers or tasks from previous runs still exist
-subprocess.check_output("merlin stop-workers", shell=True).decode("ascii")
-subprocess.check_output("merlin purge ../null_spec.yaml -f", shell=True).decode("ascii")
-
 # launch 35 merlin workflow jobs
-submit_path = "."
+script_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 concurrencies = [1, 2, 4, 8, 16, 32, 64]
 nodes = [1, 1, 1, 1, 1, 1, 2]
 samples = [1, 10, 100, 1000, 10000]
 output_path = f"null_results/run_{args.run_id}"
-os.makedirs(output_path)
+output_path = os.path.abspath(output_path)
+os.makedirs(output_path, exist_ok=True)
 for i, concurrency in enumerate(concurrencies):
     c_name = os.path.join(output_path, f"c_{concurrency}")
     if not os.path.isdir(c_name):
@@ -29,7 +27,7 @@ for i, concurrency in enumerate(concurrencies):
         os.chdir(s_name)
         submit = f"submit_{nodes[i]}_node.sbatch"
         command = f"sbatch {submit} {sample} {int(concurrency/nodes[i])}"
-        shutil.copyfile(os.path.join(submit_path, submit), submit)
+        shutil.copyfile(os.path.join(script_path, submit), submit)
         lines = subprocess.check_output(command, shell=True).decode("ascii")
         os.chdir(f"..")
     os.chdir(f"..")
