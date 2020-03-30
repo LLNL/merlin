a50c2ae97dc01909108f0c3c55d1326c50ef25ca
Benjamin Bay <bay1@llnl.gov>
2020-03-30 09:52:31 -0700

    adjustments

--- merlin/examples/workflows/null_spec/scripts/launch_jobs.py
+++ merlin/examples/workflows/null_spec/scripts/launch_jobs.py
@@ -2,6 +2,9 @@ import os
 import shutil
 import subprocess
 
+parser = argparse.ArgumentParser(description="Launch 35 merlin workflow jobs")
+parser.add_argument("run_id", type=int, help="The ID of this run")
+args = parser.parse_args()
 
 # ensure no workers or tasks from previous runs still exist
 subprocess.check_output("merlin stop-workers", shell=True).decode("ascii")
@@ -12,7 +15,8 @@ submit_path = "."
 concurrencies = [1, 2, 4, 8, 16, 32, 64]
 nodes = [1, 1, 1, 1, 1, 1, 2]
 samples = [1, 10, 100, 1000, 10000]
-output_path = "null_results/run_1"
+output_path = f"null_results/run_{args.run_id}"
+os.makedirs(output_path)
 for i, concurrency in enumerate(concurrencies):
     c_name = os.path.join(output_path, f"c_{concurrency}")
     if not os.path.isdir(c_name):
