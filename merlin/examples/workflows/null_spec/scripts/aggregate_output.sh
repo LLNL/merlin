3b0535c6ce4068783902f846b0c7c88b65d4af66
Benjamin Bay <benbayml@gmail.com>
2020-05-14 20:35:56 -0700

    script improvements to accomodate multiple node logs

--- merlin/examples/workflows/null_spec/scripts/aggregate_output.sh
+++ merlin/examples/workflows/null_spec/scripts/aggregate_output.sh
@@ -1,5 +1,5 @@
 
-runs=( 26 )
+runs=( 31 )
 concurrencies=(1 2 4 8 16 32 64)
 #samples=(100000)
 samples=(1 10 100 1000 10000)
@@ -15,7 +15,14 @@ for run in "${runs[@]}"
         for s in "${samples[@]}"
             do
             echo "c${c}_s${s} : " >> ${DATA}
-            python3 read_output.py ${read_path}/c_$c/s_$s/*.log ${read_path}/c_$c/s_$s/*.err $c $s >> ${DATA}
+            wf_path="${read_path}/c_$c/s_$s"
+
+            echo "${wf_path}/*.log"
+            echo "${wf_path}/*.err"
+            echo "$c"
+            echo "$s"
+    
+            python3 read_output.py --logfile ${wf_path}/*.log --errfile ${wf_path}/*.err --c $c --s $s >> ${DATA}
             done
         done
     perl -pi -e 's/ : \n/ : /g' ${DATA}
--- merlin/examples/workflows/null_spec/scripts/read_output.py
+++ merlin/examples/workflows/null_spec/scripts/read_output.py
@@ -2,43 +2,49 @@ import argparse
 import datetime
 import re
 import subprocess
+import glob
 
 
 # argument parsing
 parser = argparse.ArgumentParser(description="Make some samples (names of people).")
-parser.add_argument("logfile", type=str, help="celery log file")
-parser.add_argument("errfile", type=str, help="batch err file")
-parser.add_argument("c", type=int, help="concurrency")
-parser.add_argument("s", type=int, help="n of samples")
+parser.add_argument("--path", type=str, help="path to spec output")
+parser.add_argument("--c", type=int, help="concurrency")
+parser.add_argument("--s", type=int, help="n of samples")
 args = parser.parse_args()
 
+args.logfile = glob.glob(os.path.join(args.path, "*.log"))
+args.errfile = glob.glob(os.path.join(args.path, "*.err"))
 
 def single_task_times():
-    pre_lines = subprocess.check_output(
-        f'grep " succeeded in " {args.logfile}', shell=True
-    ).decode("ascii")
-
-    pre_list = pre_lines.strip().split("\n")
-
     task_durations = []
-    for line in pre_list:
-        matches = re.search(r"\d+.\d+s:", line)
-        if matches:
-            match = matches.group(0)
-            match = float(match.strip("s:"))
-            task_durations.append(match)
+    for log in args.logfile:
+        pre_lines = subprocess.check_output(
+            f'grep " succeeded in " {log}', shell=True
+        ).decode("ascii")
 
-    print(str(task_durations))
+        pre_list = pre_lines.strip().split("\n")
+
+        for line in pre_list:
+            matches = re.search(r"\d+.\d+s:", line)
+            if matches:
+                match = matches.group(0)
+                match = float(match.strip("s:"))
+                task_durations.append(match)
+
+    print(str(task_durations), end="")
 
 
 def merlin_run_time():
-    pre_line = subprocess.check_output(
-        f'grep "real" {args.errfile}', shell=True
-    ).decode("ascii")
-    pre_line = pre_line.strip()
-    matches = re.search(r"\d\.\d\d\d", pre_line)
-    match = matches[0]
-    result = float(match)
+    total = 0
+    for err in args.errfile:
+        pre_line = subprocess.check_output(
+            f'grep "real" {err}', shell=True
+        ).decode("ascii")
+        pre_line = pre_line.strip()
+        matches = re.search(r"\d\.\d\d\d", pre_line)
+        match = matches[0]
+        result = float(match)
+        total += result
     print(f"c{args.c}_s{args.s} merlin run : " + str(result))
 
 
