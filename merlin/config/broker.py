a02b13cdb9757ec3c3805dc3479b9b414aa59519
Joseph M. Koning <koning1@llnl.gov>
2020-05-18 11:16:55 -0700

    Fix spacing.

--- merlin/config/broker.py
+++ merlin/config/broker.py
@@ -266,7 +266,7 @@ def get_ssl_config():
     if not broker_ssl:
         broker_ssl = True
 
-    if broker == "rabbitmq" or broker == "rediss" or broker =="amqps":
+    if broker == "rabbitmq" or broker == "rediss" or broker == "amqps":
         return broker_ssl
 
     return False
