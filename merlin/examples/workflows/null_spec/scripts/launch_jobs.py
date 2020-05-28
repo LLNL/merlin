dffafddcd589344060c6c08c9e796136e6727dd9
Benjamin Bay <benbayml@gmail.com>
2020-05-28 10:07:04 -0700

    fixed sample vals

--- merlin/examples/workflows/null_spec/scripts/launch_jobs.py
+++ merlin/examples/workflows/null_spec/scripts/launch_jobs.py
@@ -22,7 +22,7 @@ elif "pascal" in machine:
 submit_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 concurrencies = [2**4, 2**5, 2**6, 2**7]
 nodes = [1, 1, 2, 4]
-samples = [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
+samples = [10**1, 10**2, 10**3, 10**4, 10**5, 10**6]
 output_path = os.path.join(args.output_path, f"run_{args.run_id}")
 os.makedirs(output_path, exist_ok=True)
 for i, concurrency in enumerate(concurrencies):
