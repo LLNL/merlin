2265af263cc9d67cda867e7a1064b40c7fd7d5e3
Bay <bay1@llnl.gov>
2020-07-08 12:34:44 -0600

    fixed unit tests for running offline

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -7,15 +7,17 @@ def test_broker_url():
     """
     Ensure the celery application 'broker_url' roughly matches the required pattern.
     """
-    assert re.match(
-        r"amqps:\/\/\w+:.+@jackalope\.llnl\.gov:\d+\/\w+", app.conf.broker_url
-    )
+    if app.conf.broker_url:
+        assert re.match(
+            r"amqps:\/\/\w+:.+@jackalope\.llnl\.gov:\d+\/\w+", app.conf.broker_url
+        )
 
 
 def test_result_backend():
     """
     Ensure the celery application 'result_backend' roughly matches the required pattern.
     """
-    assert re.match(
-        r"redis:\/\/\w+:\w+@jackalope\.llnl\.gov:\d+\/\w+", app.conf.result_backend
-    )
+    if app.conf.broker_url:
+        assert re.match(
+            r"redis:\/\/\w+:\w+@jackalope\.llnl\.gov:\d+\/\w+", app.conf.result_backend
+        )
