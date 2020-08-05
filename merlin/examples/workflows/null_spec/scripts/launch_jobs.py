0ceda86603eb5f927274d3890615ad5b3cc97ff2
Benjamin Bay <48391872+ben-bay@users.noreply.github.com>
2020-08-05 16:49:55 -0600

    null spec updates (for paper) (#270)
    * made samples generation actually null; now uses touch, once only
    
    * improving samples
    
    * updated job launcher
    
    * modifications
    
    * added echo
    
    * added echo
    
    * added times
    
    * fixed slurm command logic
    
    * script improvements to accomodate multiple node logs
    
    * read output script updates
    
    * read output script fixes
    
    * updated times
    
    * updated times
    
    * script updates
    
    * script changes
    
    * launcher script improvement: added run id to queue names
    
    * tweak
    
    * added account logic for quartz and pascal
    
    * path
    
    * fixed bug that mixed queues at w=32 and w=64
    
    * bugfix
    
    * tweak
    
    * added new launcher specs
    
    * added 24 hour cap
    
    * fixed sample vals
    
    * script update
    
    * launcher changes
    
    * updated merlin monitor call
    
    * updates

--- merlin/examples/workflows/null_spec/scripts/launch_jobs.py
+++ merlin/examples/workflows/null_spec/scripts/launch_jobs.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import socket
 import shutil
 import subprocess
 
@@ -8,13 +9,23 @@ parser = argparse.ArgumentParser(description="Launch 35 merlin workflow jobs")
 parser.add_argument("run_id", type=int, help="The ID of this run")
 parser.add_argument("output_path", type=str, help="the output path")
 parser.add_argument("spec_path", type=str, help="path to the spec to run")
+parser.add_argument("script_path", type=str, help="path to the make samples script")
 args = parser.parse_args()
+ 
+machine = socket.gethostbyaddr(socket.gethostname())[0]
+if "quartz" in machine:
+    machine = "quartz"
+elif "pascal" in machine:
+    machine = "pascal"
 
 # launch 35 merlin workflow jobs
-script_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
-concurrencies = [1, 2, 4, 8, 16, 32, 64]
-nodes = [1, 1, 1, 1, 1, 1, 2]
-samples = [1, 10, 100, 1000, 10000]
+submit_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+# concurrencies = [2**4, 2**5, 2**6, 2**7]
+# nodes = [1, 1, 2, 4]
+# samples = [10**1, 10**2, 10**3, 10**4, 10**5, 10**6]
+concurrencies = [2**7]
+nodes = [4]
+samples = [10**6]
 output_path = os.path.join(args.output_path, f"run_{args.run_id}")
 os.makedirs(output_path, exist_ok=True)
 for i, concurrency in enumerate(concurrencies):
@@ -22,15 +33,39 @@ for i, concurrency in enumerate(concurrencies):
     if not os.path.isdir(c_name):
         os.mkdir(c_name)
     os.chdir(c_name)
-    for sample in samples:
+    for j, sample in enumerate(samples):
         s_name = os.path.join(c_name, f"s_{sample}")
         if not os.path.isdir(s_name):
             os.mkdir(s_name)
         os.chdir(s_name)
-        submit = f"submit_{nodes[i]}_node.sbatch"
-        command = f"sbatch {submit} {sample} {int(concurrency/nodes[i])}"
-        shutil.copyfile(os.path.join(script_path, submit), submit)
-        shutil.copyfile(args.spec_path, os.path.basename(args.spec_path))
+        os.mkdir("scripts")
+        samp_per_worker = float(sample) / float(concurrency)
+        #if (samp_per_worker / 60) > times[j]:
+        #    print(f"c{concurrency}_s{sample} : {round(samp_per_worker / 60, 0)}m.\ttime: {times[j]}m.\tdiff: {round((samp_per_worker / 60) - times[j], 0)}m")
+        if (samp_per_worker / 60) < 1.0:
+            real_time = 4
+        elif (samp_per_worker / 60) < 3.0:
+            real_time = 10
+        else:
+            real_time = samp_per_worker / 60
+            real_time *= 1.5
+            real_time = int(round(real_time, 0))
+        # print(f"c{concurrency}_s{sample} : {real_time}")
+        if machine == "quartz":
+            account = "lbpm"
+            partition = "pdebug"
+        elif machine == "pascal":
+            account = "wbronze"
+            partition = "pvis"
+        if real_time > 60:
+            partition = "pbatch"
+        if real_time > 1440:
+            real_time = 1440
+        submit = "submit.sbatch"
+        command = f"sbatch -J c{concurrency}s{sample}r{args.run_id} --time {real_time} -N {nodes[i]} -p {partition} -A {account} {submit} {sample} {int(concurrency/nodes[i])} {args.run_id} {concurrency}"
+        shutil.copyfile(os.path.join(submit_path, submit), submit)
+        shutil.copyfile(args.spec_path, "spec.yaml")
+        shutil.copyfile(args.script_path, os.path.join("scripts", "make_samples.py"))
         lines = subprocess.check_output(command, shell=True).decode("ascii")
         os.chdir(f"..")
     os.chdir(f"..")
--- merlin/examples/workflows/null_spec/scripts/read_output.py
+++ merlin/examples/workflows/null_spec/scripts/read_output.py
@@ -2,83 +2,114 @@ import argparse
 import datetime
 import re
 import subprocess
+import glob
+import os
+import sys
 
 
 # argument parsing
 parser = argparse.ArgumentParser(description="Make some samples (names of people).")
-parser.add_argument("logfile", type=str, help="celery log file")
-parser.add_argument("errfile", type=str, help="batch err file")
+parser.add_argument("path", type=str, help="path to spec output")
 parser.add_argument("c", type=int, help="concurrency")
 parser.add_argument("s", type=int, help="n of samples")
 args = parser.parse_args()
 
+args.logfile = glob.glob(os.path.join(args.path, "*.log"))
+args.errfile = glob.glob(os.path.join(args.path, "*.err"))
 
 def single_task_times():
-    pre_lines = subprocess.check_output(
-        f'grep " succeeded in " {args.logfile}', shell=True
-    ).decode("ascii")
-
-    pre_list = pre_lines.strip().split("\n")
-
     task_durations = []
-    for line in pre_list:
-        matches = re.search(r"\d+.\d+s:", line)
-        if matches:
-            match = matches.group(0)
-            match = float(match.strip("s:"))
-            task_durations.append(match)
+    for log in args.logfile:
+        try:
+            pre_lines = subprocess.check_output(
+                f'grep " succeeded in " {log}', shell=True
+            ).decode("ascii")
+
+            pre_list = pre_lines.strip().split("\n")
+
+            for line in pre_list:
+                matches = re.search(r"\d+.\d+s:", line)
+                if matches:
+                    match = matches.group(0)
+                    match = float(match.strip("s:"))
+                    task_durations.append(match)
+        except:
+            continue
 
     print(str(task_durations))
 
 
 def merlin_run_time():
-    pre_line = subprocess.check_output(
-        f'grep "real" {args.errfile}', shell=True
-    ).decode("ascii")
-    pre_line = pre_line.strip()
-    matches = re.search(r"\d\.\d\d\d", pre_line)
-    match = matches[0]
-    result = float(match)
+    total = 0
+    for err in args.errfile:
+        pre_line = subprocess.check_output(
+            f'grep "real" {err}', shell=True
+        ).decode("ascii")
+        pre_line = pre_line.strip()
+        matches = re.search(r"\d\.\d\d\d", pre_line)
+        match = matches[0]
+        result = float(match)
+        total += result
     print(f"c{args.c}_s{args.s} merlin run : " + str(result))
 
 
 def start_verify_time():
-    try:
-        pre_line = subprocess.check_output(
-            f'grep -m1 "verify" {args.logfile}', shell=True
-        ).decode("ascii")
-        pre_line = pre_line.strip()
-        matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
-        match = matches[0]
-        element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
-        timestamp = datetime.datetime.timestamp(element)
-        print(f"c{args.c}_s{args.s} start verify : " + str(timestamp))
-    except BaseException:
-        print(f"c{args.c}_s{args.s} start verify : ERROR")
+    all_timestamps = []
+    for log in args.logfile:
+        try:
+            pre_line = subprocess.check_output(
+                f'grep -m1 "verify" {log}', shell=True
+            ).decode("ascii")
+            pre_line = pre_line.strip()
+            matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
+            match = matches[0]
+            element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
+            timestamp = datetime.datetime.timestamp(element)
+            all_timestamps.append(timestamp)
+        except:
+            continue
+        try:
+            print(f"c{args.c}_s{args.s} start verify : " + str(all_timestamps[0]))
+        except BaseException:
+            print(f"c{args.c}_s{args.s} start verify : ERROR")
 
 
 def start_run_workers_time():
-    pre_line = subprocess.check_output(
-        f'grep -m1 "" {args.logfile}', shell=True
-    ).decode("ascii")
-    pre_line = pre_line.strip()
-    matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
-    match = matches[0]
-    element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
-    timestamp = datetime.datetime.timestamp(element)
-    print(f"c{args.c}_s{args.s} start run-workers : " + str(timestamp))
+    all_timestamps = []
+    for log in args.logfile:
+        try:
+            pre_line = subprocess.check_output(
+                f'grep -m1 "" {log}', shell=True
+            ).decode("ascii")
+            pre_line = pre_line.strip()
+            matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
+            match = matches[0]
+            element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
+            timestamp = datetime.datetime.timestamp(element)
+            all_timestamps.append(timestamp)
+        except:
+            continue
+    earliest = min(all_timestamps)
+    print(f"c{args.c}_s{args.s} start run-workers : " + str(earliest))
 
 
 def start_sample1_time():
-    pre_line = subprocess.check_output(
-        f"grep -m1 \"Executing step 'null_step'\" {args.logfile}", shell=True
-    ).decode("ascii")
-    pre_line = pre_line.strip()
-    matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
-    match = matches[0]
-    element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
-    timestamp = datetime.datetime.timestamp(element)
-    print(f"c{args.c}_s{args.s} start samp1 : " + str(timestamp))
+    all_timestamps = []
+    for log in args.logfile:
+        try:
+            pre_line = subprocess.check_output(
+                f"grep -m1 \"Executing step 'null_step'\" {log}", shell=True
+            ).decode("ascii")
+            pre_line = pre_line.strip()
+            matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
+            match = matches[0]
+            element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
+            timestamp = datetime.datetime.timestamp(element)
+            all_timestamps.append(timestamp)
+        except:
+            continue
+    earliest = min(all_timestamps)
+    print(f"c{args.c}_s{args.s} start samp1 : " + str(earliest))
 
 
 def main():
