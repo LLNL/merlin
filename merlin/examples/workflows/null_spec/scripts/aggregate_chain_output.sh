7e99bdf68d28df1da3c624fdbfa42eba0d0c71eb
Benjamin Bay <benbayml@gmail.com>
2020-09-01 08:35:09 -0700

    adapting scripts for null chain

--- merlin/examples/workflows/null_spec/scripts/aggregate_chain_output.sh
+++ merlin/examples/workflows/null_spec/scripts/aggregate_chain_output.sh
@@ -1,29 +1,73 @@
 
-# $1    run_id
-# $2    read_path
+# $1    read_path
 
+
+# > TODO: expand for all runs?
+r=2
 samples=(1 10 100 1000 10000 100000)
 
-read_path="$2/run_${1}"
-NULL_CHAINS=( $(find $read_path -type d -name "null_chain_*" -maxdepth 1) )
+read_path="$1/run_${r}"
+#NULL_CHAINS=( $(find $read_path -type d -name "null_chain_*" -maxdepth 1) )
+#echo ${NULL_CHAINS}
+LOGS=( $(find $read_path -type f -name "*.log" -maxdepth 1) )
 
-echo ${NULL_CHAINS}
 
-DATA=my_data${1}.yaml
+DATA_DIR=my_data${r}
+mkdir ${DATA_DIR}
+# for each log file, split it into sections based on sample
+i=0
+for log in "${LOGS[@]}"
+    do
+    echo "${log}"
+    csplit -f ${DATA_DIR}/split_log_${i}_ -z ${log} /"Step 'verify' in "/ '{*}'
+    i=$(( $i+1 ))
+    done
+
+SPLIT_LOGS=( $(find $DATA_DIR -type f -name "split_log_*_*" -maxdepth 1) )
+#       samples
+# 0     1
+# 1     10
+# 2     100
+# 3     1000
+# 4     10000
+# 5     100000
+# 6     n/a
+
+DATA=${DATA_DIR}/my_data${r}.yaml
 touch ${DATA}
 
-c=1
-for chain in "${NULL_CHAINS[@]}"
-    do
-    s=1
-    for s in $samples
-        do
-        echo "c${c}_s${s} : " >> ${DATA}
-        wf_path="${chain}/"
-        echo ${wf_path}
-        python3 read_output.py ${wf_path}/ $c $s >> ${DATA}
-        s=$(( $s*10 ))
-        done
+s=1
+i=0
+c=2 #make this dynamic
+#for chain in "${NULL_CHAINS[@]}"
+#for s in "${samples[@]}"
+#    do
+    #s=${samples[${i}]}
+#    echo "c${c}_s${s} : " >> ${DATA}
+    #wf_path="${chain}/"
+echo "${read_path}"
+python3 read_output_chain.py ${read_path}/ $c >> ${DATA}
+    #s=$(( $s*10 ))
+    #i=$(( $i+1 ))
+    #done
+#c=$(( $c*2 ))
+
+
+#c=1
+#for chain in "${NULL_CHAINS[@]}"
+#    do
+
+#    wf_path="${chain}/"
+#    echo ${wf_path}
+
+    #s=1
+    #for s in $samples
+    #    do
+    #    echo "c${c}_s${s} : " >> ${DATA}
+    #    wf_path="${chain}/"
+        python3 read_output_chain.py ${wf_path}/ $c >> ${DATA}
+    #    s=$(( $s*10 ))
+    #    done
     c=$(( $c*2 ))
-    done
+#    done
 perl -pi -e 's/ : \n/ : /g' ${DATA}
---
+++ merlin/examples/workflows/null_spec/scripts/read_output_chain.py
@@ -0,0 +1,148 @@
+import argparse
+import datetime
+import glob
+import os
+import re
+import subprocess
+import sys
+
+
+SAMPLES = [1, 10, 100, 1000, 10000, 100000]
+
+# argument parsing
+parser = argparse.ArgumentParser(description="Make some samples (names of people).")
+parser.add_argument("path", type=str, help="path to spec output")
+parser.add_argument("c", type=int, help="concurrency")
+args = parser.parse_args()
+
+print(args.path)
+
+logpattern = os.path.join(args.path, "split_log_*")
+errpattern = os.path.join(args.path, "*.err")
+args.logfile = glob.glob(logpattern)
+args.errfile = glob.glob(errpattern)
+
+if len(args.logfile) == 0:
+    print(f"{logpattern} returned no glob matches!")
+    sys.exit()
+if len(args.errfile) == 0:
+    print(f"{errpattern} returned no glob matches!")
+    sys.exit()
+
+for log in args.logfile:
+    print(log)
+sys.exit()
+
+def single_task_times():
+    task_durations = []
+    for log in args.logfile:
+        try:
+            pre_lines = subprocess.check_output(
+                f'grep " succeeded in " {log}', shell=True
+            ).decode("ascii")
+
+            pre_list = pre_lines.strip().split("\n")
+
+            for line in pre_list:
+                matches = re.search(r"\d+.\d+s:", line)
+                if matches:
+                    match = matches.group(0)
+                    match = float(match.strip("s:"))
+                    task_durations.append(match)
+        except:
+            continue
+
+    #print("c${c}_s${s} :" + str(task_durations))
+    print("c${args.c}_sall :" + str(task_durations))
+
+
+def merlin_run_time(samp):
+    total = 0
+    for err in args.errfile:
+        pre_line = subprocess.check_output(f'grep "real" {err}', shell=True).decode(
+            "ascii"
+        )
+        pre_line = pre_line.strip()
+        matches = re.search(r"\d\.\d\d\d", pre_line)
+        match = matches[0]
+        result = float(match)
+        total += result
+    try:
+        print(f"c{args.c}_s{samp} merlin run : " + str(result))
+    except:
+        result = None
+        print(
+            f"c{args.c}_s{samp} merlin run : ERROR -- result={result}, args.errfile={args.errfile}"
+        )
+
+
+def start_verify_time(samp):
+    all_timestamps = []
+    for log in args.logfile:
+        try:
+            pre_line = subprocess.check_output(
+                f'grep -m1 "verify" {log}', shell=True
+            ).decode("ascii")
+            pre_line = pre_line.strip()
+            matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
+            match = matches[0]
+            element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
+            timestamp = datetime.datetime.timestamp(element)
+            all_timestamps.append(timestamp)
+        except:
+            continue
+        try:
+            print(f"c{args.c}_s{samp} start verify : " + str(all_timestamps[0]))
+        except BaseException:
+            print(f"c{args.c}_s{samp} start verify : ERROR")
+
+
+def start_run_workers_time(samp):
+    all_timestamps = []
+    for log in args.logfile:
+        try:
+            pre_line = subprocess.check_output(f'grep -m1 "" {log}', shell=True).decode(
+                "ascii"
+            )
+            pre_line = pre_line.strip()
+            matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
+            match = matches[0]
+            element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
+            timestamp = datetime.datetime.timestamp(element)
+            all_timestamps.append(timestamp)
+        except:
+            continue
+    earliest = min(all_timestamps)
+    print(f"c{args.c}_s{samp} start run-workers : " + str(earliest))
+
+
+def start_sample1_time(samp):
+    all_timestamps = []
+    for log in args.logfile:
+        try:
+            pre_line = subprocess.check_output(
+                f"grep -m1 \"Executing step 'null_step'\" {log}", shell=True
+            ).decode("ascii")
+            pre_line = pre_line.strip()
+            matches = re.search(r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d", pre_line)
+            match = matches[0]
+            element = datetime.datetime.strptime(match, "%Y-%m-%d %H:%M:%S,%f")
+            timestamp = datetime.datetime.timestamp(element)
+            all_timestamps.append(timestamp)
+        except:
+            continue
+    earliest = min(all_timestamps)
+    print(f"c{args.c}_s{samp} start samp1 : " + str(earliest))
+
+
+def main():
+    for samp in SAMPLES:
+        single_task_times()
+        merlin_run_time(samp)
+        start_verify_time(samp)
+        start_run_workers_time(samp)
+        start_sample1_time(samp)
+
+
+if __name__ == "__main__":
+    main()
