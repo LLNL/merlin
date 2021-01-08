bfdc30b48dfd02c467e60d5a00eb2229392047b5
Benjamin Bay <bay1@llnl.gov>
2021-01-08 14:56:05 -0800

    1.7.8

--- merlin/__init__.py
+++ merlin/__init__.py
@@ -6,7 +6,7 @@
 #
 # LLNL-CODE-797170
 # All rights reserved.
-# This file is part of Merlin, Version: 1.7.7.
+# This file is part of Merlin, Version: 1.7.8.
 #
 # For details, see https://github.com/LLNL/merlin.
 #
@@ -38,7 +38,7 @@ import os
 import sys
 
 
-__version__ = "1.7.7"
+__version__ = "1.7.8"
 VERSION = __version__
 PATH_TO_PROJ = os.path.join(os.path.dirname(__file__), "")
 
