cca8f0d96ed8d77d5aab3c4bbe640dbd30b6801c
Benjamin Bay <bay1@llnl.gov>
2021-06-08 15:54:14 -0700

    fixed typo

--- merlin/celery.py
+++ merlin/celery.py
@@ -85,7 +85,7 @@ task_priority_defaults = {
     "task_queue_max_priority": get_priority(Priority.high),
     "task_default_priority": get_priority(Priority.mid),
 }
-if CONGIG.broker.name.lower() == "redis":
+if CONFIG.broker.name.lower() == "redis":
     app.conf.broker_transport_options = {
         "priority_steps": list(range(1, 11)),
         "queue_order_strategy": "priority",
