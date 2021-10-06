c3afb59be9fb564a21b61163fdc5157b159b9473
Alexander Cameron Winter <winter27@quartz1148.llnl.gov>
2021-10-05 17:17:15 -0700

    added typing and documentation in the remote workflows *.py files.

--- merlin/examples/workflows/remote_feature_demo/scripts/pgen.py
+++ merlin/examples/workflows/remote_feature_demo/scripts/pgen.py
@@ -1,13 +1,19 @@
+from typing import Dict, List, Union
+
 from maestrowf.datastructures.core import ParameterGenerator
 
 
-def get_custom_generator(env, **kwargs):
-    p_gen = ParameterGenerator()
-    params = {
+def get_custom_generator(env, **kwargs) -> ParameterGenerator:
+    """
+    Generates parameters to feed at the CL to hello_world.py.
+    """
+    p_gen: ParameterGenerator = ParameterGenerator()
+    params: Dict[str, Union[List[float], str]] = {
         "X2": {"values": [1 / i for i in range(3, 6)], "label": "X2.%%"},
         "N_NEW": {"values": [2 ** i for i in range(1, 4)], "label": "N_NEW.%%"},
     }
-
+    key: str
+    value: Union[List[float], str]
     for key, value in params.items():
         p_gen.add_parameter(key, value["values"], value["label"])
 
