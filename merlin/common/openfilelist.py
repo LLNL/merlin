430d90981ab832a7aa3538fe0f2af482729a0ff3
Luc Peterson <peterson76@llnl.gov>
2022-02-03 14:15:35 -0800

    Bump to version 1.8.4

--- merlin/common/openfilelist.py
+++ merlin/common/openfilelist.py
@@ -8,7 +8,7 @@
 #
 # LLNL-CODE-797170
 # All rights reserved.
-# This file is part of Merlin, Version: 1.8.3.
+# This file is part of Merlin, Version: 1.8.4.
 #
 # For details, see https://github.com/LLNL/merlin.
 #
