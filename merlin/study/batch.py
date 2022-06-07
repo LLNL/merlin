98469c3085e0041e6427743c5fed6f74fb5f4713
Joe Koning <koning@users.noreply.github.com>
2022-06-07 10:21:53 -0700

    Allow for flux exec arguments to limit the number of celery workers. (#366)
    * Added the flux_exec batch argument to allow for flux exec arguments,
    e.g. flux_exec: flux exec -r "0-1" to run celery workers only on
    ranks 0 and 1 of a multi-rank allocation.
    
    * Remove period.

--- merlin/study/batch.py
+++ merlin/study/batch.py
@@ -175,7 +175,7 @@ def batch_worker_launch(
 
         flux_exec: str = ""
         if flux_exec_workers:
-            flux_exec = "flux exec"
+            flux_exec = get_yaml_var(batch, "flux_exec", "flux exec")
 
         if "/" in flux_path:
             flux_path += "/"
