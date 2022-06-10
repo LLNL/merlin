9381afebff39e8b3f071dade8d9fbfeb5c98a55a
Ryan Lee <lee1098@llnl.gov>
2022-06-10 12:40:48 -0700

    Added some info messages to give user feedback when adding and removing users

--- merlin/server/server_commands.py
+++ merlin/server/server_commands.py
@@ -85,8 +85,10 @@ def config_server(args: Namespace) -> None:
         # Log the user in a file
         if redis_users.add_user(user=args.add_user[0], password=args.add_user[1]):
             redis_users.write()
+            LOG.info(f"Added user {args.add_user[0]} to merlin server")
             # Create a new user in container
             if get_server_status() == ServerStatus.RUNNING:
+                LOG.info(f"Adding user to current merlin server instance")
                 redis_users.apply_to_redis(redis_config.get_ip_address(), redis_config.get_port(), redis_config.get_password())
         else:
             LOG.error(f"User '{args.add_user[0]}' already exisits within current users")
@@ -95,8 +97,10 @@ def config_server(args: Namespace) -> None:
         # Remove user from file
         if redis_users.remove_user(args.remove_user):
             redis_users.write()
+            LOG.info(f"Removed user {args.remove_user} to merlin server")
             # Remove user from container
             if get_server_status() == ServerStatus.RUNNING:
+                LOG.info(f"Removing user to current merlin server instance")
                 redis_users.apply_to_redis(redis_config.get_ip_address(), redis_config.get_port(), redis_config.get_password())
         else:
             LOG.error(f"User '{args.remove_user}' doesn't exist within current users.")
