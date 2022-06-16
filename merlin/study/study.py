45ffd39eb9aa1a679a99c7707f59ebfa33999163
Jim Gaffney <gaffney3@llnl.gov>
2022-06-16 12:35:19 -0700

    changing parameter names

--- merlin/study/study.py
+++ merlin/study/study.py
@@ -107,7 +107,9 @@ class MerlinStudy:
             "MERLIN_SOFT_FAIL": str(int(ReturnCode.SOFT_FAIL)),
             "MERLIN_HARD_FAIL": str(int(ReturnCode.HARD_FAIL)),
             "MERLIN_RETRY": str(int(ReturnCode.RETRY)),
-            "MERLIN_ALL_SAMPLES": " ".join([ "$({})".format(k) for k in self.get_sample_labels(from_spec=self.original_spec)]),
+            # below will be substituted for sample values on execution
+            "MERLIN_SAMPLE_VECTOR": " ".join([ "$({})".format(k) for k in self.get_sample_labels(from_spec=self.original_spec)]),
+            "MERLIN_SAMPLE_NAMES": " ".join(self.get_sample_labels(from_spec=self.original_spec)),
             "MERLIN_SPEC": os.path.join(
                     self.info,
                     self.original_spec.description["name"].replace(" ", "_") + ".expanded.yaml"
