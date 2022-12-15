d6f86c99e95e9dc7a3e4375222fdc3e30dc17633
Brian Gunnarson <brianfunnarson14@gmail.com>
2022-12-15 12:22:49 -0800

    pypi changes to drop py 3.6 and add 3.11

--- setup.py
+++ setup.py
@@ -96,11 +96,11 @@ setup(
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
     ],
     keywords="machine learning workflow",
     url="https://github.com/LLNL/merlin",
