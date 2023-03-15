37661756d084169d3b9c947f72918bd916ff5180
Brian Gunnarson <brianfunnarson14@gmail.com>
2023-03-15 14:56:12 -0700

    Update integration test suite and add stop-workers tests
    Update integration test suite and add stop-workers tests
    
    Update integration test suite and add stop-workers tests
    
    modify changelog to show test changes
    
    update copyright year and CI explanation

--- merlin/config/results_backend.py
+++ merlin/config/results_backend.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2022, Lawrence Livermore National Security, LLC.
+# Copyright (c) 2023, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory
 # Written by the Merlin dev team, listed in the CONTRIBUTORS file.
 # <merlin@llnl.gov>
