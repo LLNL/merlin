c511105ff105bc81bd681cb528cd82234e0a36f1
Brian Gunnarson <brianfunnarson14@gmail.com>
2023-06-27 16:29:48 -0700

    fix naming error with latex_engine

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,7 +122,7 @@ htmlhelp_basename = "Merlindoc"
 
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_engine = "latexpdf"
+latex_engine = "pdflatex"
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
