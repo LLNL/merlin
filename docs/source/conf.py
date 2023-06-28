04b369e71b8c106cd6a39a01d6714da8d4585ce7
Brian Gunnarson <brianfunnarson14@gmail.com>
2023-06-28 07:18:57 -0700

    attempting to add a logo to the pdf build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -138,6 +138,8 @@ latex_elements = {
     'figure_align': 'htbp',
 }
 
+latex_logo = "../images/merlin.png"
+
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
