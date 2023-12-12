54a31bce2bf5a5bad5dd9ff25b7bfd32476e0aa8
Brian Gunnarson <brianfunnarson14@gmail.com>
2023-12-12 10:52:34 -0800

    add unit tests for util_sampling

--- merlin/common/util_sampling.py
+++ merlin/common/util_sampling.py
@@ -35,6 +35,7 @@ Utility functions for sampling.
 import numpy as np
 
 
+# TODO should we move this to merlin-spellbook?
 def scale_samples(samples_norm, limits, limits_norm=(0, 1), do_log=False):
     """Scale samples to new limits, either log10 or linearly.
 
