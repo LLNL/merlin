07f558cf13aa9e533a75b42d7e16988ab07760d8
Brian Gunnarson <brianfunnarson14@gmail.com>
2024-05-24 13:36:36 -0700

    add tests to ensure chord error isn't raised

--- tests/integration/definitions.py
+++ tests/integration/definitions.py
@@ -128,6 +128,7 @@ def define_tests():  # pylint: disable=R0914,R0915
     lsf = f"{examples}/lsf/lsf_par.yaml"
     mul_workers_demo = f"{dev_examples}/multiple_workers.yaml"
     cli_substitution_wf = f"{test_specs}/cli_substitution_test.yaml"
+    chord_err_wf = f"{test_specs}/chord_err.yaml"
 
     # Other shortcuts
     black = "black --check --target-version py36"
@@ -827,6 +828,27 @@ def define_tests():  # pylint: disable=R0914,R0915
             "run type": "distributed",
         },
     }
+    distributed_error_checks = {
+        "check chord error continues wf": {
+            "cmds": [f"{workers} {chord_err_wf} --vars OUTPUT_PATH=./{OUTPUT_DIR}", f"{run} {chord_err_wf} --vars OUTPUT_PATH=./{OUTPUT_DIR}; sleep 5"],
+            "conditions": [
+                HasReturnCode(),
+                PathExists(  # Check that the sample that's supposed to raise an error actually raises an error
+                    f"{OUTPUT_DIR}/process_samples/01/MERLIN_FINISHED",
+                    negate=True,
+                ),
+                StepFileExists(  # Check that step 2 is actually started and completes
+                    "step_2",
+                    "MERLIN_FINISHED",
+                    "chord_err",
+                    OUTPUT_DIR,
+                ),
+            ],
+            "run type": "distributed",
+            "cleanup": KILL_WORKERS,
+            "num procs": 2,
+        }
+    }
 
     # combine and return test dictionaries
     all_tests = {}
@@ -849,6 +871,7 @@ def define_tests():  # pylint: disable=R0914,R0915
         stop_workers_tests,
         query_workers_tests,
         distributed_tests,
+        distributed_error_checks,
     ]:
         all_tests.update(test_dict)
 
