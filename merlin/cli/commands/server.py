c80a60e0d629f84cabaefddd7af36a0f41545f51
Brian Gunnarson <brianfunnarson14@gmail.com>
2025-06-16 17:24:33 -0700

    add copyright to the top of all new files

--- merlin/cli/commands/server.py
+++ merlin/cli/commands/server.py
@@ -1,3 +1,9 @@
+##############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other Merlin
+# Project developers. See top-level LICENSE and COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Merlin.
+##############################################################################
+
 """
 Merlin CLI server command module.
 
