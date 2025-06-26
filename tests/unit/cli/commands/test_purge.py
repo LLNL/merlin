9b7c2b0ba96a0e466f2534af8ef3670df3166d05
Brian Gunnarson <brianfunnarson14@gmail.com>
2025-06-26 11:47:46 -0700

    add tests for queue-info, restart, and run-workers

--- tests/unit/cli/commands/test_purge.py
+++ tests/unit/cli/commands/test_purge.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other Merlin
+# Project developers. See top-level LICENSE and COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Merlin.
+##############################################################################
+
+"""
+Tests for the `purge.py` file of the `cli/` folder.
+"""
+
 import logging
 from argparse import ArgumentParser, Namespace
 
