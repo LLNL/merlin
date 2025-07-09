3a0011cd8404381f03dd0b1d22c9e8b494cfa3c5
MarcusHsieh <marcus.j.hsieh@gmail.com>
2025-07-09 09:17:25 -0700

    Fix styling issues

--- tests/unit/task_servers/test_task_server_interface.py
+++ tests/unit/task_servers/test_task_server_interface.py
@@ -1,3 +1,9 @@
+##############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other Merlin
+# Project developers. See top-level LICENSE and COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Merlin.
+##############################################################################
+
 """
 Unit tests for TaskServerInterface abstract base class.
 
