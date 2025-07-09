e4949353d207a8e387190081b891266391be3a9f
Marcus Jake Hsieh <hsieh10@ruby961.llnl.gov>
2025-07-08 19:23:04 -0700

    TaskServerInterface implementation

---
+++ tests/unit/task_servers/__init__.py
@@ -0,0 +1,9 @@
+"""
+Unit tests for the Merlin task server abstraction layer.
+
+This package contains tests for:
+- TaskServerInterface abstract base class
+- TaskServerFactory registration and creation
+- Task server implementations (Celery, Kafka, etc.)
+- Configuration validation and error handling
+""" 
