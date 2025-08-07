1a8c79d4882b6056f787625017c30805c03dc921
MarcusHsieh <marcus.j.hsieh@gmail.com>
2025-08-06 17:31:10 -0700

    + Python 3.8+ compatibility + Performance limit tests + Pytest performance markers

---
+++ tests/performance/__init__.py
@@ -0,0 +1,13 @@
+##############################################################################
+# Copyright (c) Lawrence Livermore National Security, LLC and other Merlin
+# Project developers. See top-level LICENSE and COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Merlin.
+##############################################################################
+
+"""
+Performance tests for Merlin components.
+
+This package contains performance tests that focus on testing Merlin's behavior
+with large datasets and high-load scenarios. These tests are separate from
+regular unit/integration tests and may take longer to run.
+"""
