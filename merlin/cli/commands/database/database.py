9e555821a66175aed6b749cc34f94d2c1cae1548
Brian Gunnarson <brianfunnarson14@gmail.com>
2025-10-06 18:03:32 -0700

    add 'db' alias for database command

--- merlin/cli/commands/database/database.py
+++ merlin/cli/commands/database/database.py
@@ -61,6 +61,7 @@ class DatabaseCommand(CommandEntryPoint):
         """
         database: ArgumentParser = subparsers.add_parser(
             "database",
+            aliases=["db"],
             help="Interact with Merlin's database.",
             formatter_class=ArgumentDefaultsHelpFormatter,
         )
